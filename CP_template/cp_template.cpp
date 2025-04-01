b8e7df57e0eb317848f31b517a7756b79e337cc6
Gaurav-k-Gupta <gauravgupta0954@gmail.com>
2025-04-02 00:47:31 +0530

    added seg tree class

--- CP_template/cp_template.cpp
+++ CP_template/cp_template.cpp
@@ -27,36 +27,64 @@ const ll INF = 1e9;
 
 class seg_tree{
     private:
+
     int n;
     vll t;
-    vi a;
 
-    void buildHelper( int v , int tl , int tr ){
+
+    ll combine( ll a , ll b ){
+        return a + b;
+    }
+
+    void buildHelper( int v , int tl , int tr , vi & a){
         if (tl == tr) {
-            t[v] = ;
-        } else {
+            t[v] = a[tl];
+        }
+        else{
             int tm = (tl + tr) / 2;
-            build(a, v*2, tl, tm);
-            build(a, v*2+1, tm+1, tr);
+            buildHelper(v*2 , tl , tm , a);
+            buildHelper( v*2 + 1 , tm+1 , tr , a );
             t[v] = combine(t[v*2], t[v*2+1]);
         }
     }
 
-    public:
-    seg_tree( int n , vi &a ){
-        t.resize( 4*n );
-        this->n = n;
-        this->a = a;
+    void pointUpdateHelper( int v , int tl , int tr , int pos , int val ){
+        if( tl == tr ){
+            t[v] = val;
+        }
+        else{
+            int tm = ( tl + tr )/2;
+            if( pos <= tm ) pointUpdateHelper( v*2 , tl , tm , pos , val );
+            else pointUpdateHelper( v*2 + 1 , tm+1 , tr , pos , val );
+
+            t[v] = combine( t[v*2] , t[v*2 + 1]);
+        }
     }
 
-    void build(){
-        buildHelper( 0 , n-1);
-    }    
+    ll rangeQueryHelper( int v , int tl , int tr , int l , int r ){
+        if( l > r ) return 0;
+        if( l == tl && r == tr ) return t[v];
+        int tm = ( tl + tr )/2;
+        return rangeQueryHelper( v*2 , tl , tm , l , min( tm , r ) ) + rangeQueryHelper( v*2 + 1 , tm+1 , tr , max( tm+1 , l ) , r);
+    }
+
+    public:
+
+    seg_tree( vi &a ){
+        this->n = a.size();
+        t.resize( 4*n + 1 );
+        buildHelper( 1 , 0 , n-1 , a );
+    }
+
+    void pointUpdate( int pos , int val ){
+        pointUpdateHelper( 1 , 0 , n-1 , pos , val );
+    }
+
+    ll rangeQuery( int l , int r ){
+        return rangeQueryHelper( 1 , 0 , n-1 , l , r );
+    }
+        
 };
-
-
-
-
 
 
 
@@ -117,6 +145,12 @@ ll Pow( ll x , ll exp ){
 
 
 void solve(){
+    vi a = { 2 , 4 , 5 , 1 , 10 , -5 , -2 , 3};
+    seg_tree t(a);
+
+    cout<<t.rangeQuery(3 , 6)<<endl;
+    cout<<t.rangeQuery(0 , 7)<<endl;
+    cout<<t.rangeQuery(1 , 7)<<endl;
 
 }
 
@@ -127,6 +161,6 @@ int main(){
     int t;
     cin>>t;
     while( t-- ){
-
+        solve();
     }
 }
